085479aef8a79d6f32f234c6f27d92440d2f2fcc
Michael Kelly <cybernationnews@gmail.com>
2022-05-14 16:04:27 -0500

    Add WeaponController
    Add weapon animation logic to Cutlass.

---
+++ MMFC/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject Cutlass;
+    public bool can_attack;
+    public float cooldown = 1.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (can_attack)
+            {
+                CutlassSwing();
+            }
+        }
+    }
+
+    public void CutlassSwing()
+    {
+        can_attack = false;
+        Animator attack_anim = Cutlass.GetComponent<Animator>();
+        attack_anim.SetTrigger("Melee");
+        StartCoroutine(ResetCooldown());
+
+    }
+
+    IEnumerator ResetCooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        can_attack = true;
+    }
+
+
+}
