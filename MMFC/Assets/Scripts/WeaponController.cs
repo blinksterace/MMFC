d40d7e1d2bd7d868166ea94137b040c23c71bc2d
Aneesh Akella <aneesh.akella.aa@gmail.com>
2024-05-04 15:53:26 -0500

    Fixes to the combat system and map upgrade
    Changed lighting and added more outside sections to the map. Fixed the issue with combat where it would get stuck after first animation by adding idle tags and changing countdowns for the click timing.

--- MMFC/Assets/Scripts/WeaponController.cs
+++ MMFC/Assets/Scripts/WeaponController.cs
@@ -7,7 +7,7 @@ public class WeaponController : MonoBehaviour
     public GameObject Cutlass;
     private bool downAndAttack = false;
     public bool can_attack;
-    public float cooldown = 0.72f;
+    public float cooldown = 1.0f;
     public Collider swordBox;
     public bool mouseDown = false;
     [SerializeField] private GameObject bloodps = null;
@@ -16,6 +16,7 @@ public class WeaponController : MonoBehaviour
     public float attackDamage = 10f;
     public LayerMask attackLayer;
     public Camera fpsCam;
+    private int clickCount = 0;
 
 
 
@@ -34,25 +35,81 @@ public class WeaponController : MonoBehaviour
         downAndAttack = Input.GetMouseButtonDown(1) && can_attack;
         if (downAndAttack)
         {
+           Debug.Log("clickcount:" + clickCount);     
             CutlassSwing();
+            StartCoroutine(WaitForClick());
+            clickCount++;
+            
         }
-
-        if(downAndAttack == true)
-            Debug.Log(downAndAttack);
     }
 
+
+    private IEnumerator WaitForClick()
+    {
+        float timer = 0;
+        while (timer < .2f)
+        {
+            // If the player clicks again within .2 second, increment clickCount and break the loop
+            if (Input.GetMouseButtonDown(1))
+            {
+                CutlassSwing();
+                break;
+            }
+
+            // Increment the timer by the time since the last frame
+            timer += Time.deltaTime;
+
+            // Yield control back to the Unity engine until the next frame
+            yield return null;
+        }
+
+        // If the player didn't click again within .2 second, reset clickCount to 0
+        if (timer >= .2f )
+        {
+            clickCount = 0;
+            ResetCooldown();
+            yield break;
+        } 
+    }
     public void CutlassSwing()
     {
+        Debug.Log("Click Count: " + clickCount);
         // Disabling attack
-        can_attack = false;
+        //can_attack = false;
 
         // Animation
         Animator attack_anim = Cutlass.GetComponent<Animator>();
-        attack_anim.SetTrigger("Melee");
-        Invoke(nameof(AttackRaycast), attackDelay);
-        
-        // Cooldown
-        StartCoroutine(ResetCooldown());
+
+        // Perform attack based on clickCount
+        switch (clickCount)
+        {
+            case 0:
+                // First attack
+                attack_anim.SetTrigger("Melee");
+                Invoke(nameof(AttackRaycast), attackDelay);
+                StartCoroutine(ResetCooldown());
+                break;
+            case 1:
+                // Second attack
+                 attack_anim.SetTrigger("SecondMelee");
+                Invoke(nameof(AttackRaycast), attackDelay);
+                StartCoroutine(ResetCooldown());
+                clickCount = 0;
+                break;
+            /*case 3:
+                // Third attack
+                attack_anim.SetTrigger("Melee");
+                Invoke(nameof(AttackRaycast), attackDelay);
+                clickCount = 0; // Reset the combo
+                break;
+            */
+            default:
+                // Reset the combo if clickCount is not 1, 2, or 3
+                //clickCount = 0;
+                break;
+        }
+            // Cooldown
+
 
     }
 
@@ -74,6 +131,8 @@ public class WeaponController : MonoBehaviour
     IEnumerator ResetCooldown()
     {
         yield return new WaitForSeconds(cooldown);
+        Animator attack_anim = Cutlass.GetComponent<Animator>();
+        attack_anim.SetTrigger("Idle");
         can_attack = true;
     }
 
